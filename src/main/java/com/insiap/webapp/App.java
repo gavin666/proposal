41cdedc9994597e68beab152a1dd92e8a43273e2
gavin.gao <gavin.gao@e-bao.cn>
2017-02-17 11:13:45 +0800

    rename app name

--- src/main/java/com/insiap/webapp/App.java
+++ src/main/java/com/insiap/webapp/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProposalApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProposalApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
