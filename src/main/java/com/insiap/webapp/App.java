dc376646a919a9eb519d18d6f7e72d3d0d6540f0
gavin.gao <gavin.gao@e-bao.cn>
2017-02-20 16:55:44 +0800

    增加ssl配置以及重定向

--- src/main/java/com/insiap/webapp/App.java
+++ src/main/java/com/insiap/webapp/App.java
@@ -1,12 +1,46 @@
 package com.insiap.webapp;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
-
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainerFactory() {
+		TomcatEmbeddedServletContainerFactory factory =
+				new TomcatEmbeddedServletContainerFactory() {
+					@Override
+					protected void postProcessContext(Context context) {
+						//SecurityConstraint必须存在，可以通过其为不同的URL设置不同的重定向策略。
+						SecurityConstraint securityConstraint = new SecurityConstraint();
+						securityConstraint.setUserConstraint("CONFIDENTIAL");
+						SecurityCollection collection = new SecurityCollection();
+						collection.addPattern("/*");
+						securityConstraint.addCollection(collection);
+						context.addConstraint(securityConstraint);
+					}
+				};
+		factory.addAdditionalTomcatConnectors(createHttpConnector());
+		return factory;
+	}
+
+	private Connector createHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setSecure(false);
+		connector.setPort(8080);
+		connector.setRedirectPort(8443);
+		return connector;
 	}
 }
